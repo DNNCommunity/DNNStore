fbc2aed3f7c5aad355b9fc2b08e70331687e6d88
valadas <info@danielvaladas.com>
2018-10-06 01:24:11 -0400

    Added packaging scripts and resolved sql issues for EVS

---
+++ NugetPackages/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetPackages
+{
+    // This class does nothing intentionnaly,
+    // This project is only used to restore nuget packages only once of all other projects
+    public class Class1
+    {
+    }
+}
